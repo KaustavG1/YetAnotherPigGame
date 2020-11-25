2bd8b6e284c90be9892d2c0a8090fc3506853230
KaustavG1 <mail2kg2@gmail.com>
2020-11-26 00:13:12 +0530

    Added Rules

--- Pig-Game/app.js
+++ Pig-Game/app.js
@@ -11,6 +11,12 @@ GAME RULES:
 
 let current = 0;
 
+/*
+Init Conditions:
+
+- All elements (both UI and variables) should return to their initial conditions. So call the _init_() function.
+
+*/
 
 function _inti_() {
     document.querySelector('#score-0').textContent = '0';
@@ -23,10 +29,26 @@ function _inti_() {
 
 _inti_();
 
+/* 
+The New Game Button:
+
+- All elements (both UI and variables) should return to their initial conditions. So call the _init_() function.
+
+*/
+
 const newGame = () => {
     console.log('Starting a new Game');
     _inti_();
 }
+
+/*
+The Roll Dice Button:
+
+- Generate a random number 
+- If random number is not 1 Show the appropriate dice image
+- If random number is 1 then clear current player's current score and call the hold function to change player
+
+*/
 
 const rollDice = () => {
     const rand = Math.floor(Math.random() * 6) + 1;
@@ -34,8 +56,15 @@ const rollDice = () => {
     document.querySelector('.dice').style.display = 'block';
     current += rand;
     console.log(`Current is ${current} and rand is ${rand}`);
-    document.querySelector()
+    document.querySelector('#current-0').textContent = `${current}`
 }
+
+/*
+The Hold Button:
+
+- TO-DO
+
+*/
 
 const hold = () => {
     console.log('Holding');
